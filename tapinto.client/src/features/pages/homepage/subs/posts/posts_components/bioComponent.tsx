8f157e5b77b8790af7c09918cbe66ad78b69c750
Meshack-Mlangeni <meshackmlangeni@hotmail.com>
2024-06-25 23:11:04 +0200

    Update

--- tapinto.client/src/features/pages/homepage/subs/posts/posts_components/bioComponent.tsx
+++ tapinto.client/src/features/pages/homepage/subs/posts/posts_components/bioComponent.tsx
@@ -1,52 +1,52 @@
-import Avatar from '@mui/joy/Avatar';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import Typography from '@mui/joy/Typography';
-import { Sheet } from '@mui/joy';
+import Avatar from "@mui/joy/Avatar";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import { Sheet } from "@mui/joy";
 
 export default function Bio() {
   return (
     <Card
       sx={{
-        maxWidth: '100%'
+        maxWidth: "100%",
       }}
     >
-      <CardContent sx={{ justifyContent:"center", textAlign: 'center' }}>
-        <Avatar sx={{ '--Avatar-size': '4rem', mx:"auto" }} />
+      <CardContent sx={{ justifyContent: "center", textAlign: "center" }}>
+        <Avatar sx={{ "--Avatar-size": "4rem", mx: "auto" }} />
         <Typography level="title-lg">Meshack Mlangeni</Typography>
-        <Typography level="body-sm" >
+        <Typography level="body-sm">
           I started learning React in January and I'm tired!
         </Typography>
         <Sheet
-            sx={{
-              bgcolor: 'background.level1',
-              borderRadius: 'sm',
-              p: 1.5,
-              mt:1,
-              display: 'flex',
-              gap: 2,
-              '& > div': { flex: 1 },
-            }}
-          >
-            <div>
-              <Typography level="body-xs" fontWeight="lg">
-                Posts
-              </Typography>
-              <Typography fontWeight="lg">00</Typography>
-            </div>
-            <div>
-              <Typography level="body-xs" fontWeight="lg">
-                Groups
-              </Typography>
-              <Typography fontWeight="lg">00</Typography>
-            </div>
-            <div>
-              <Typography level="body-xs" fontWeight="lg">
-                Rating
-              </Typography>
-              <Typography fontWeight="lg">0.0</Typography>
-            </div>
-          </Sheet>
+          sx={{
+            bgcolor: "background.level1",
+            borderRadius: "sm",
+            p: 1.5,
+            mt: 1,
+            display: "flex",
+            gap: 2,
+            "& > div": { flex: 1 },
+          }}
+        >
+          <div>
+            <Typography level="body-xs" fontWeight="lg">
+              Posts
+            </Typography>
+            <Typography fontWeight="lg">00</Typography>
+          </div>
+          <div>
+            <Typography level="body-xs" fontWeight="lg">
+              Groups
+            </Typography>
+            <Typography fontWeight="lg">00</Typography>
+          </div>
+          <div>
+            <Typography level="body-xs" fontWeight="lg">
+              Rating
+            </Typography>
+            <Typography fontWeight="lg">0.0</Typography>
+          </div>
+        </Sheet>
       </CardContent>
     </Card>
   );
--- tapinto.client/src/features/pages/homepage/subs/posts/posts_components/myGroupsComponent.tsx
+++ tapinto.client/src/features/pages/homepage/subs/posts/posts_components/myGroupsComponent.tsx
@@ -2,12 +2,8 @@ import Avatar from "@mui/joy/Avatar";
 import Card from "@mui/joy/Card";
 import CardContent from "@mui/joy/CardContent";
 import Typography from "@mui/joy/Typography";
-import {
-  Divider,
-  List,
-  ListItemButton,
-  ListItemDecorator,
-} from "@mui/joy";
+import { Divider, List, ListItemButton, ListItemDecorator } from "@mui/joy";
+import { Groups2Rounded } from "@mui/icons-material";
 
 export default function MyGroups() {
   return (
@@ -19,11 +15,12 @@ export default function MyGroups() {
     >
       <CardContent>
         <Typography level="h3" variant="plain">
+          <Groups2Rounded sx={{ position:"relative", top: -3, mr: 2 }} />
           My Group(s)
         </Typography>
         <Divider />
         <List>
-          <ListItemButton>
+          <ListItemButton sx={{borderRadius: 8}}>
             <ListItemDecorator>
               <Avatar size="sm">T1</Avatar>
             </ListItemDecorator>
@@ -31,7 +28,7 @@ export default function MyGroups() {
           </ListItemButton>
           <ListItemButton>
             <ListItemDecorator>
-            <Avatar size="sm">T2</Avatar>
+              <Avatar size="sm">T2</Avatar>
             </ListItemDecorator>
             Test 1
           </ListItemButton>
