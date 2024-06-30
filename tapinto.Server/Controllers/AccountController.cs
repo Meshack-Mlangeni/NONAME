faf0a380d2701c0c30ec64e8b543e19c3683d39d
Sabelokhoza <161332464+Sabelokhoza@users.noreply.github.com>
2024-06-30 17:46:43 +0200

    signin and  token based token implemented

--- tapinto.Server/Controllers/AccountController.cs
+++ tapinto.Server/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,33 +14,78 @@ using tapinto.Server.Models;
 
 namespace tapinto.Server.Controllers
 {
+   
     public class AccountController : BaseApiController
     {
         private readonly ILogger<AccountController> _logger;
         private readonly AppDbContext context;
-        private readonly UserManager<User> userManager;
+        private readonly UserManager<User> _userManager;
+        private readonly AuthController _authController;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        
 
-        public AccountController(ILogger<AccountController> logger, AppDbContext _context, UserManager<User> _userManager)
+
+        public AccountController(ILogger<AccountController> logger, AppDbContext _context,
+            UserManager<User> userManager, AuthController authController , RoleManager<IdentityRole> roleManager)
         {
             _logger = logger;
             context = _context;
-            userManager = _userManager;
+            _userManager = userManager;
+            _authController = authController;
+            _roleManager = roleManager;
+
         }
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
-            var user = userManager.Users.Where(u => u.Email == loginDto.Email).FirstOrDefaultAsync();
-            if(user != null){
-                return new UserDto(await user);
+            var user = await _userManager.Users.Where(u => u.Email == loginDto.Email).FirstOrDefaultAsync();
+            
+            if(user != null && await _userManager.CheckPasswordAsync(user, loginDto.Password)){
+              
+                var userDto = new UserDto( user);
+                var token = await _authController.Auntheticate(user);
+                userDto.Token = token.accessToken;
+                
+                return userDto;
             }
+
             return BadRequest();
         }
 
         [HttpPost("register")]
-        public IActionResult Register(RegisterDto registerDto)
+        public async Task<IActionResult> Register(RegisterDto registerDto)
         {
-            _logger.Log(LogLevel.Information, "Successfully registered!");
+            //var user = new User{
+            //    Email = registerDto.Email,
+            //    FirstName = registerDto.FirstName,
+            //    LastName = registerDto.LastName
+            //};
+            //var result = await userManager.CreateAsync(user, registerDto.Password);
+
+            //if(result.Succeeded){
+            //    foreach(var error in result.Errors){
+            //        ModelState.AddModelError(error.Code, error.Description);
+            //    }
+
+            //    return ValidationProblem();
+            //}
+
+            //_logger.Log(LogLevel.Information, "Successfully registered!");
+
+            //await userManager.AddToRoleAsync(user, registerDto.RegisterAs);
+
             return Ok();
+        }
+
+        [Authorize]
+        [HttpGet("currentUser")]
+        public async Task<ActionResult<UserDto>> GetUser()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var userDto = new UserDto( user);
+            var Token = await _authController.Auntheticate(user);
+            userDto.Token = Token.accessToken;
+            return userDto;
         }
 
     }
---
+++ tapinto.Server/Models/Token.cs
@@ -0,0 +1,8 @@
+﻿namespace tapinto.Server.Models
+{
+    public class Token
+    {
+        public string accessToken { get; set; }
+        public DateTime expiresAt { get; set; }
+    }
+}
