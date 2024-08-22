914a2533452edfd57dba1fc7c0c249d1169b398e
Sabelokhoza <161332464+Sabelokhoza@users.noreply.github.com>
2024-08-22 17:57:21 +0200

    Implemented ExceptionMiddleware and NotFound Exception

---
+++ tapinto.Server/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using tapinto.Server.Exceptions;
+using tapinto.Server.Models.ExceptionModels;
+
+namespace tapinto.Server.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, ILogger<ExceptionMiddleware> logger)
+        {
+            try
+            {
+                logger.LogError($"Something went wrong processing {httpContext.Request.Path}");
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+               await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            httpContext.Response.ContentType = "application/json";
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            var errorDetails = new ErrorDetails
+            {
+                ErrorType = "Failure",
+                ErrorMessage = ex.Message,
+            };
+
+            switch (ex)
+            {
+                case NotFoundException notFoundException:
+                    statusCode = HttpStatusCode.NotFound;
+                    errorDetails.ErrorType = "Not Found";
+                    break;
+
+                default:
+                    break;
+            }
+
+            var response = JsonConvert.SerializeObject(errorDetails);
+            httpContext.Response.StatusCode = (int)statusCode;
+
+            return httpContext.Response.WriteAsync(response);
+
+        }
+    }
+}
